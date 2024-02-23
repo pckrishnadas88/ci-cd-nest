d8545221a3e2b87c43200cec7a0d8734a2cedfcd
pckrishnadas88 <pckrishnadas88@gmail.com>
2024-02-23 15:59:12 +0530

    comment added

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+// dso
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
-bootstrap();
+bootstrap(); 
